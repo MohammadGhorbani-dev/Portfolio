819961d0d5f09fdf2a6a8d22ac386428db84e5cb
MohammadGhorbani-dev <mohamad.ghorbani.official@gmail.com>
2023-12-06 03:10:58 +0330

    init and add libraries

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import stylisRTLPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import { HelmetProvider, Helmet } from "react-helmet-async";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
+import { Button } from "@mui/material";
+
+// Create Custom Theme
+const theme = createTheme({
+  direction: "rtl",
+  typography: {
+    fontFamily: "vazir,roboto",
+  },
+});
+
+// Create RTL Cache
+const cacheRTL = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, stylisRTLPlugin],
+});
+
 function App() {
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <CacheProvider value={cacheRTL}>
+      <ThemeProvider theme={theme}>
+        <HelmetProvider>
+          <Helmet>
+            <title>وب سایت شخصی محمد قربانی</title>
+          </Helmet>
+          <div className="App">
+            <Button variant="contained">سلام</Button>{" "}
+            <Button variant="contained">Hello</Button>
+          </div>
+        </HelmetProvider>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
